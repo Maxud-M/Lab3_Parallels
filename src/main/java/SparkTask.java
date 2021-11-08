b99547d1f740623e32b5e35cc6d28c5f4d74db96
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-08 22:22:26 +0300

    Scripted auto-commit on change (2021-11-08 22:22:26) by gitwatch.sh

--- src/main/java/SparkTask.java
+++ src/main/java/SparkTask.java
@@ -4,7 +4,8 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class SparkTask {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
-        JavaSparkContext sc = new JavaSparkContext()
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        
 
 
     }
