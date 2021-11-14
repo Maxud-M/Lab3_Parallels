2a91776874986b515a58dc3c60788ae5ceb83a6b
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-14 22:25:22 +0300

    Scripted auto-commit on change (2021-11-14 22:25:22) by gitwatch.sh

--- src/main/java/SparkTask.java
+++ src/main/java/SparkTask.java
@@ -8,14 +8,15 @@ public class SparkTask {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> distFile = sc.textFile(args[0]);
+        JavaRDD<String> flights = sc.textFile(args[0]);
         JavaPairRDD<Tuple2<String, String>, FlightData> pairRDD = distFile.mapToPair(
                 s -> new Tuple2<Tuple2<String, String>, FlightData>(
                         new Tuple2<String, String>(s.split(",")[11], s.split(",")[14]),
                         FlightReader.parseFlightData(s)
                 )
         );
-        
+        JavaRDD<String> airports = sc.textFile(args[1]);
+        JavaRDD<String, >
 
 
 
