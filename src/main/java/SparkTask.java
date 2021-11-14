f970e7886c1e0cb296861bb695103758308c4e4f
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-14 22:59:54 +0300

    Scripted auto-commit on change (2021-11-14 22:59:54) by gitwatch.sh

--- src/main/java/SparkTask.java
+++ src/main/java/SparkTask.java
@@ -25,7 +25,9 @@ public class SparkTask {
         );
         Map<String, Integer> airportsMap = pairAirportsRDD.collectAsMap();
         final Broadcast<Map<String, Integer>> airportsBroadcasted = sc.broadcast(airportsMap);
-        pairFlightsRDD.map()
+        pairFlightsRDD.map(T -> {
+
+        });
 
 
     }
