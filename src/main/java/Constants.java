8f04325176daa2ba610b0c10397bdc6bb9a5855a
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-15 11:51:09 +0300

    Scripted auto-commit on change (2021-11-15 11:51:09) by gitwatch.sh

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,5 +2,5 @@ public class Constants {
     public static final String DELIMITER = ",";
 
     public static final String QUOTE = "\"";
-    public static final String 
+    public static final String NEW_LINE_CHARACTER
 }
