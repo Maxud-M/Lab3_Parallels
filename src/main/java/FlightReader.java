fa57867a3ba5f2ca4500f104a4c739f1b774372d
Maxud-M <maksud.maksudov.00@mail.ru>
2021-11-15 11:40:41 +0300

    Scripted auto-commit on change (2021-11-15 11:40:41) by gitwatch.sh

--- src/main/java/FlightReader.java
+++ src/main/java/FlightReader.java
@@ -1,9 +1,12 @@
 
 
 public class FlightReader {
+
+
+
     static public FlightData parseFlightData(String inputData) {
         String[] data = inputData.split(",");
-        float delay = (!data[18].equals(""))? Float.parseFloat(data[18]) : 0F;
+        float delay = (!data[18].equals(""))? Float.parseFloat(data[17]) : 0F;
         float cancelled = (!data[19].equals(""))? Float.parseFloat(data[19]) : 0F;
         return new FlightData(delay, cancelled);
     }
